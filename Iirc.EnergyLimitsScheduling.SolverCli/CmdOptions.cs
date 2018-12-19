9b5a22371763f3cbe55d8e2834702f81e2ebedd6
Istvan Modos <istvan.modos@cvut.cz>
2018-12-19 13:34:13 +0100

    Remove unused option in SolverCli

--- Iirc.EnergyLimitsScheduling.SolverCli/CmdOptions.cs
+++ Iirc.EnergyLimitsScheduling.SolverCli/CmdOptions.cs
@@ -15,8 +15,5 @@ namespace Iirc.EnergyLimitsScheduling.SolverCli
 
         [Value(1, Required = true, MetaValue = "INSTANCE_PATH", HelpText = "Path to the instance file.")]
         public string InstancePath { get; set; }
-
-        [Value(1, Required = true, MetaValue = "RESULT_PATH", HelpText = "Path to the result file.")]
-        public string ResultPath { get; set; }
     }
 }
